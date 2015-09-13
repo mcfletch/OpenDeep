33a875b3b1ce97dda1c4b352df090f654e2da5b7
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-09-13 19:59:38 -0400

    Add code to load sph files for processing...

--- opendeep/data/standard_datasets/tedlium/sph.py
+++ opendeep/data/standard_datasets/tedlium/sph.py
@@ -1,4 +1,8 @@
-"""Loader for .sph files such as found in TEDLium dataset"""
+"""Loader for NIST .sph files *as found in TEDLium dataset*
+
+Header is a 1024-byte null padded textual format, rest is samples
+in the format specified in the header.
+"""
 import logging
 import numpy
 log = logging.getLogger(__name__)
@@ -6,6 +10,8 @@ log = logging.getLogger(__name__)
 def parse_sph_header( fh ):
     """Read the file-format header for an sph file"""
     file_format = {
+        # there are the sphere defaults, though the real content
+        # isn't going to have this format, normally...
         'sample_rate':8000, 
         'channel_count':1, 
         'sample_byte_format': '01', # little-endian
@@ -23,11 +29,52 @@ def parse_sph_header( fh ):
                 if format == '-i':
                     value = int(value, 10)
                 file_format[key] = value 
+    if file_format['sample_byte_format'] == '01':
+        file_format['big_endian'] = True
+    else:
+        file_format['big_endian'] = False
     return file_format
 
-def sph_audio_data( fh, header ):
-    """Given header and a file handle, load the audio data in expected format
+def load_audio_data( fh, format ):
+    """Given format and a file handle, load the audio data in expected format
     
     Assumes the TED LIUM data-format is loosely followed, so
     if the file isn't quite correct we'll just error out.
     """
+    fh.seek(1024)
+    array = numpy.fromfile( fh, dtype = ('>' if format['big_endian'] else '<') + 'H' )
+    if not array.dtype.byteorder == '=':
+        array.byteswap( True )
+    return array
+
+def _parse_all_sphs( filepath ):
+    """Trivial "does it crash" manual test operation
+    
+    This just confirms that all of the TED LIUM corpus can
+    be format-parsed. It doesn't necessarily mean that the parsing 
+    is *correct*
+    """
+    import os
+    count = 0
+    for path,dirs,files in os.walk(filepath):
+        sphs = [x for x in files if x.lower().endswith('.sph')]
+        for sph in sphs:
+            source = open(os.path.join(path,sph))
+            format = parse_sph_header( source )
+            count += 1
+            assert source.tell() == 1024, source.tell()
+            source.seek(0)
+            content = source.read(1024)
+            assert format['sample_rate'] in (8000,16000), content
+            assert format['sample_coding'] == 'pcm', format
+            assert format['channel_count'] == 1, (sph,format)
+            assert format['sample_n_bytes'] == 2, format
+            #print(format)
+            log.info("Loading audio for: %s",sph)
+            load_audio_data( source, format )
+    log.info("Parsed format for %s files", count )
+
+if __name__ == '__main__':
+    import sys
+    logging.basicConfig(level=logging.INFO)
+    _parse_all_sphs( sys.argv[1] )
