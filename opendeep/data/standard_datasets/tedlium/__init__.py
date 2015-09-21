4f6f8cfed75d58ba4994fd3a87ada3af967331a7
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-09-20 22:38:46 -0400

    Have a default location that's reasonable (at least on Linux)

--- opendeep/data/standard_datasets/tedlium/__init__.py
+++ opendeep/data/standard_datasets/tedlium/__init__.py
@@ -1,7 +1,9 @@
 """Loader for the TEDLium dataset
 
 Note that this dataset is *not* directly downloadable,
-as it requires a registration step. It is *also* a 
-40GB dataset, which is a rather large download on 
+as it requires a registration step. It is *also* a
+40GB dataset, which is a rather large download on
 slow connections.
 """
+import os
+DEFAULT_DATASET_PATH = os.path.expanduser('~/.config/opendeep/datasets')
