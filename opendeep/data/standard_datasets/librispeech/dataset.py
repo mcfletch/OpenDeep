6502cc8267699a826b08a2d357e7ec4647d20936
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-10-09 12:12:08 -0400

    Use precise sizes to check for completed downloads and resume-if-not

--- opendeep/data/standard_datasets/librispeech/dataset.py
+++ opendeep/data/standard_datasets/librispeech/dataset.py
@@ -1,5 +1,9 @@
 """OpenDeep Dataset Implementation for LibriSpeech Corpus"""
 import logging, os
+try:
+    long 
+except NameError:
+    long = int
 from . import DEFAULT_LIBRISPEECH_DATASET_PATH
 from opendeep.utils import file_ops
 log = logging.getLogger( __name__ )
@@ -23,16 +27,15 @@ TRAIN_OTHER_500 = 'train-other-500.tar.gz'
 
 INTRO_DISCLAIMERS = 'intro-disclaimers.tar.gz'
 
-
 FILE_SIZES = {
-    DEV_CLEAN: '337MB',
-    DEV_OTHER: '314MB',
-    TEST_CLEAN: '346MB',
-    TEST_OTHER: '328MB',
-    TRAIN_CLEAN_100:'6.3GB',
-    TRAIN_CLEAN_360:'23GB',
-    TRAIN_OTHER_500:'30GB',
-    INTRO_DISCLAIMERS: '695MB',
+    DEV_CLEAN: 337926286,
+    DEV_OTHER: 314305928,
+    TEST_CLEAN: 346663984,
+    TEST_OTHER: 328757843,
+    TRAIN_CLEAN_100: 6387309499,
+    TRAIN_CLEAN_360: 23049477885,
+    TRAIN_OTHER_500: 30593501606,
+    INTRO_DISCLAIMERS: 695964615,
 }
 ALL_FILES = sorted(list(FILE_SIZES.keys()))
 
@@ -44,11 +47,12 @@ def ensure_downloads(files,base_url=BASE_URL,target_dir=DEFAULT_LIBRISPEECH_DATA
         final_filename = os.path.join( target_dir, filename )
         log.info("Ensuring download: %s", final_filename)
         filesize = FILE_SIZES.get( filename, 'Unknown Size')
+        size_desc = file_ops.human_bytes(filesize) if isinstance(filesize,(long,int)) else filesize
         without_extension = final_filename[:-7]
         if not os.path.exists( without_extension ):
-            if not os.path.exists( final_filename ):
+            if (not os.path.exists( final_filename )) or not( os.stat(final_filename).st_size == filesize):
                 final_url = base_url + filename
-                log.info("Need to download %s (%s)", final_url,filesize )
+                log.info("Need to download %s (%s)", final_url,size_desc )
                 file_ops.download_file(
                     final_url,
                     final_filename,
